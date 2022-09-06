f626ecf342a6a5e5bcd22934825b61a346f1922d
Grégori Sabel <seriosabel57@gmail.com>
2022-09-06 10:53:10 -0300

    responsive ++

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,63 +7,51 @@ import content from '../../content.json'
 
 export function Home () {
 
-  const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-      max: 8,
-      min: 4
-    }
-  })
-
-  const loremTitle = lorem.generateWords(3)
-  const loremText = lorem.generateParagraphs(1)
-
   const shapeBGColor = useColorModeValue('gray.100', 'gray.80')
   return (
-    <Box bg={shapeBGColor}>
-    <Box position='absolute' right='40px' top='20px' overflow='hidden'>
-      <CheckTheme />
-    </Box>
+    <Flex w='100%' justify='center'>
+      <Flex 
+        maxW='800px' 
+        flexDir='column' 
+        align='center' 
+        mb='80px' 
+        marginX={['15px', '20px', '40px']} 
+        bg={shapeBGColor}
+      >
+        <Box position='absolute' right={['20px','40px']} top='20px' overflow='hidden'>
+          <CheckTheme />
+        </Box>
 
+        <Text fontSize='2xl' fontWeight='bold' mt={['40px', '60px','80px']}>
+          Projects
+        </Text>
+        { content.projects.map( project => (
+          <Box key={project.title}>
+            <a href={project.github} target="_blank" rel="noreferrer">
+                <Block  title={project.title} text={project.text} hasGithub/>
+            </a>
+          </Box>
+        ))}
 
-      <Flex w='100%' align='center' flexDir='column'>
-        <Flex 
-          maxW='800px' 
-          flexDir='column' 
-          mb='80px' 
-          marginX={['10px', '20px', '40px']} 
-          align='center'
-        >
+        <Text fontSize='2xl' fontWeight='bold' mt='80px'>
+          Experience
+        </Text>
+        { content.experience.map( experience => (
+          <Block key={experience.title} title={experience.title} text={experience.text}/>
+        ))}
 
-            <Text fontSize='2xl' fontWeight='bold' mt='80px'>
-              Projects
-            </Text>
-            { content.projects.map( project => (
-              <Box key={project.title}>
-                <a href={project.github} target="_blank" rel="noreferrer">
-                    <Block  title={project.title} text={project.text} hasGithub/>
-                </a>
-              </Box>
-            ))}
-
-            <Text fontSize='2xl' fontWeight='bold' mt='80px'>
-              Experience
-            </Text>
-            { content.experience.map( experience => (
-              <Block key={experience.title} title={experience.title} text={experience.text}/>
-            ))}
-
-            <Text fontSize='2xl' fontWeight='bold' mt='80px'>
-              Other habilities
-            </Text>
-            <Flex gap='40px'>
-              { content.habilities.map( habilities => (
-                <Block key={habilities.title} title={habilities.title} text={habilities.text}/>
-              ))}
-            </Flex>
-
+        <Text fontSize='2xl' fontWeight='bold' mt='80px'>
+          Other habilities
+        </Text>
+        <Flex gap={['0px', '0px', '40px']} flexDir={['column', 'column', 'row']}>
+          { content.habilities.map( habilities => (
+            <Block key={habilities.title} title={habilities.title} text={habilities.text}/>
+          ))}
         </Flex>
-      </Flex>      
-    </Box>
+
+      </Flex>
+    </Flex>   
+      
   )
 }
 
